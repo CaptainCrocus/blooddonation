6633a1dd5c4c6f79d80907cf7f4b06425b3dab1f
Captain Crocus <captaincrocus@gmail.com>
2018-01-13 22:54:03 +0400

    Express-handlebars adding, templates, views

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
+const handlebars = require('express-handlebars').create({
+	defaultLayout: 'main'
+});
 
 var app = express();
 
-app.get('/', (req, res) => res.send('Hello, bladerunner'));
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
+app.get('/', (req, res) => {
+	res.render('login');
+});
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
