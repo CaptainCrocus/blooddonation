6a810a205f176d89489c79661e069e96ce444116
Captain Crocus <captaincrocus@gmail.com>
2018-01-13 21:01:36 +0400

    Index.js

--- index.js
+++ index.js
@@ -1,40 +1,9 @@
 const express = require('express');
-const path = require('path');
 const PORT = process.env.PORT || 5000;
-var mongoose = require('mongoose');
 
 var app = express();
-const connectionString = "mongodb://blood:blood@ds151207.mlab.com:51207/heroku_mf2mgkt8";
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-mongoose.connect(connectionString);
+app.get('/', (req, res) => res.send('Hello, bladerunner'));
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', console.log.bind(console, 'connection success!'));
 
-var personSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    middleName: String,
-    passport: String,
-    fin: String,
-    address: String,
-    phone: String,
-    mobile: String,
-    sex: Boolean,
-    description: String
-});
-
-var Peoples = mongoose.model('Peoples', personSchema, 'Peoples');
-
-Peoples.find(function (err, persons) {
-  if (err) 
-  	return console.error(err);
-  console.log(persons);
-});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
